f156a8635373ccf5f156937b04865c2088a2a5b4
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-09 20:56:11 -0300

    ...

--- src/rotas/index.tsx
+++ src/rotas/index.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom"
+import AreaLogada from "../paginas/AreaLogada"
 import Home from "../paginas/Home"
 import PaginaBase from "../paginas/PaginaBase"
+import Pedidos from "../paginas/Pedido"
+
 
 
 const Rotas = () => {
     return (<Routes>
       <Route path='/' element={<PaginaBase />}>
         <Route path='/' element={<Home />} />
+        <Route path='/minha-conta' element={<AreaLogada />}>
+            <Route path="pedidos" element={<Pedidos />} />
+         </Route>
       </Route>
     </Routes>)
 }
