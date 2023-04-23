dbaca906b4446292ae998e51a560be15370334b5
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-22 22:33:20 -0300

    ...

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { IAutor } from "../interfaces/IAutor";
+import { ICategoria } from "../interfaces/ICategoria";
+import { ILivro } from "../interfaces/ILivro";
 
 const http = axios.create({
     baseURL: 'http://localhost:8000',
@@ -22,3 +25,47 @@ http.interceptors.request.use(function (config) {
   });
 
 export default http
+
+export const obterCategoriaPorSlug = async (slug: string) => {
+  const resposta = await http.get<ICategoria[]>('categorias', {
+    params: {
+      slug
+    }
+  })
+  return resposta.data[0]
+}
+
+export const obterLivrosDestaque = async (tipo: string) => {
+  const resposta = await http.get<ILivro[]>(`public/${tipo}`)
+  return resposta.data
+}
+
+export const obterProdutosDaCategoria = async (categoria: ICategoria) => {
+  const resposta = await http.get<ILivro[]>('livros', {
+    params: {
+      categoria: categoria.id
+    }
+  })
+  return resposta.data
+}
+
+export const obterAutor = async (autorId: number) => {
+  try {
+    const resposta = await http.get<IAutor>(`autores/${autorId}`)
+    return resposta.data
+  } catch (error) {
+    console.log('Não foi possivel obter o autor!')
+  }
+}
+
+export const obterLivro = async (slug: string) => {
+  const resposta = await http.get<ILivro[]>('livros', {
+    params: {
+      slug
+    }
+  })
+  if (resposta.data.length === 0) {
+    return null
+  }
+  return resposta.data[0]
+}
