43888ed0e496d1a843a01ecdaea37328dfed9e82
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-04-22 23:35:26 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Rotas from './rotas';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Rotas from "./rotas";
+import ABApolloClient from "./componentes/ABApolloClient";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Rotas />
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+    return (
+        <ABApolloClient>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Rotas />
+                </BrowserRouter>
+            </QueryClientProvider>
+        </ABApolloClient>
+    );
 }
 
 export default App;
--- src/componentes/ListaLivros/index.tsx
+++ src/componentes/ListaLivros/index.tsx
@@ -1,20 +1,52 @@
-import { ICategoria } from "../../interfaces/ICategoria"
-import { useQuery } from '@tanstack/react-query'
-import { obterProdutosDaCategoria } from "../../http"
-import CardLivro from "../CardLivro"
+import { gql, useQuery } from "@apollo/client";
+import { ICategoria } from "../../interfaces/ICategoria";
+// import { useQuery } from '@tanstack/react-query'
+// import { obterProdutosDaCategoria } from "../../http"
+import CardLivro from "../CardLivro";
 
-import './ListaLivros.css'
+import "./ListaLivros.css";
+import { ILivro } from "../../interfaces/ILivro";
 
 interface ListaLivrosProps {
-    categoria: ICategoria
+    categoria: ICategoria;
 }
+
+const OBTER_LIVROS = gql`
+query ObterLivros($categoriaId: Int) {
+    livros(categoriaId: $categoriaId) {
+      id
+      slug
+      titulo
+      imagemCapa
+      opcoesCompra {
+        id
+        preco
+      }
+    }
+  }
+`;
 
 const ListaLivros = ({ categoria }: ListaLivrosProps) => {
+    // const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
+    const { data } = useQuery<{ livros: ILivro[] }>(OBTER_LIVROS, {
+        variables: {
+            categoriaId: categoria.id
+        }
+    });
 
-    const { data: produtos } = useQuery(['buscaLivrosPorCategoria', categoria], () => obterProdutosDaCategoria(categoria))
-    return <section className="livros">
-        {produtos?.map(livro => <CardLivro livro={livro} key={livro.id} />)}
-    </section>
-}
+    return (
+        // <section className="livros">
+        //     {produtos?.map((livro) => (
+        //         <CardLivro livro={livro} key={livro.id} />
+        //     ))}
+        // </section>
 
-export default ListaLivros
+        <section className="livros">
+            {data?.livros.map((livro) => (
+                <CardLivro livro={livro} key={livro.id} />
+            ))}
+        </section>
+    );
+};
+
+export default ListaLivros;
